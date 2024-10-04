304e1539ff5fbfd59ebd495b6c5e37f1603a7d5a
Binh Thanh <binhnt.it28@gmail.com>
2024-10-04 17:30:02 +0700

    commit

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,6 +9,8 @@ import { io } from "socket.io-client";
 const socket = io('http://localhost:3000');
 socket.on("connect", () => {
   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});socket.on("test", (ar) => {
+  console.log(ar); // x8WIv7-mJelg7on_ALbx
 });
 
 </script>
